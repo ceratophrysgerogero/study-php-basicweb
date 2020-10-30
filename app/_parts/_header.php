07e73004846582869882ce97b38ee0b12ffebf7b
iwasakieisuke <iwasaki@centsys.jp>
2020-10-30 19:03:57 +0900

    共通で表示していたタイトルをほかのページで設定できるようにした

--- app/_parts/_header.php
+++ app/_parts/_header.php
@@ -1,9 +1,8 @@
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= htmlspecialchars($title, ENT_QUOTES, "UTF-8"); ?></title>
 </head>
 <body>
