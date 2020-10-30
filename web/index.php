eabe6ae36b1b7df9ff19c0eb211b280da7a8c9ae
iwasakieisuke <iwasaki@centsys.jp>
2020-10-30 16:15:20 +0900

    ヘッダー、フッター、ファイル作成

--- web/index.php
+++ web/index.php
@@ -1,18 +1,9 @@
 <?php
-
 require('../app/functions.php');
-
+include('../app/_parts/_header.php');
 ?>
 
+<p>Hello</p>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>Hello</p>
-</body>
-</html>
+<?php
+include('../app/_parts/_footer.php');
