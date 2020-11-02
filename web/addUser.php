3cf30303d30ae4572d4036e87ae8f3af9c64cc36
iwasakieisuke <iwasaki@centsys.jp>
2020-11-02 18:56:34 +0900

    ユーザー登録画面(仮

---
+++ web/addUser.php
@@ -0,0 +1,13 @@
+<form action="insert.php" method="post">
+  <table>
+    <tr>
+      <td>NAME</td>
+      <td><input type="text" name="name"></td>
+    </tr>
+    <tr>
+      <td>PASSWORD</td>
+      <td><input type="text" name="password"></td>
+    </tr>
+  </table>
+  <input type="submit">
+</form>
