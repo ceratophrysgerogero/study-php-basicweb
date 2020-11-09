296f9e920bfdf79399021abd7b864081b4ee6baf
iwasakieisuke <iwasaki@centsys.jp>
2020-11-09 19:09:43 +0900

    csrf対策　セッション生成とトークン一致メソッド作成

---
+++ app/_function/functions.php
@@ -0,0 +1,30 @@
+<?php
+class CsrfValidator
+{
+
+  const HASH_ALGO = 'sha256';
+
+  public static function generate()
+  {
+    // セッションが有効だけれどもセッションが存在しない場合
+    if (session_status() === PHP_SESSION_NONE) {
+      echo 'セッションがありません。';
+    }
+    //セッションを作成してハッシュ化する
+    return hash(self::HASH_ALGO, session_id());
+  }
+
+  public static function validate($token)
+  {
+    $success = self::generate() === $token;
+    if ($success) {
+      echo 'CSRFトークンが一致しません。';
+      if (!$_SERVER['REQUEST_URI'] === $_SERVER['REQUEST_URI']) {
+        echo '3秒後にログイン画面に遷移します。';
+        //遷移させるだけでいいのか？？
+        echo '<meta http-equiv="refresh" content=" 3; url=index.php">';
+      }
+    }
+    return $success;
+  }
+}
