9b845c425a2a20d00602e91d3a9963d1542f8543
iwasakieisuke <iwasaki@centsys.jp>
2020-11-10 14:10:07 +0900

    最初にインサートするレコードの名前が前のレコードの履歴からもってきてしまうバグ修正、追加でステータスが0　無効アカウント作成できるようにした

--- mysql-textfile/create_test_user.sql
+++ mysql-textfile/create_test_user.sql
@@ -1,10 +1,16 @@
 DELIMITER //
 CREATE PROCEDURE sample01()
 BEGIN
-  DECLARE v1 INT DEFAULT 100;
+  DECLARE v1 INT DEFAULT 99;
+  DECLARE v2 INT DEFAULT 199;
+  INSERT INTO user (name,password,mail,status,created_at,updated_at) VALUES (CONCAT('name',1),0,uuid(),1,now(),now());
   WHILE v1 > 0 DO
   INSERT INTO user (name,password,mail,status,created_at,updated_at) VALUES (CONCAT('name', last_insert_id()+1),0,uuid(),1,now(),now());
-    SET v1 = v1 - 1;
+  SET v1 = v1 - 1;
+  END WHILE;
+  WHILE v2 > 99 DO
+  INSERT INTO user (name,password,mail,status,created_at,updated_at) VALUES (CONCAT('name', last_insert_id()+1),0,uuid(),0,now(),now());
+  SET v2 = v2 - 1;
   END WHILE;
 END;
 //
@@ -24,6 +30,8 @@ mail=ランダムユニーク数値(prykeyのため)
 status = 0
 created_at updated_at = 今
 を100件追加する
+status = 1
+を100件追加する
 
 解説
 ストアドプロシージャ機能と呼ばれる
@@ -33,6 +41,4 @@ delimiterは 「;」(デリミタ　終端文字)を //に変更して実行す�
 先頭の//と終盤の//は単純に書き方
 BEGIN ~ END 内に, 複数行のプロシージャを記述
 
-ちょっとしたバグ
-テーブルを作成しなおした後に実行すると前の挿入履歴をlast_insert_idがもってきてしまうので最初のnameが1にならない
 */
